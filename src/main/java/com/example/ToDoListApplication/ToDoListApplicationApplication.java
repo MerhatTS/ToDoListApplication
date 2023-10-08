c345530320f2ff755377164a34708e3969fc5275
Merhat <johndoe@example.com>
2023-10-08 17:02:17 +0600

    20231008 commit master

--- src/main/java/com/example/ToDoListApplication/ToDoListApplicationApplication.java
+++ src/main/java/com/example/ToDoListApplication/ToDoListApplicationApplication.java
@@ -2,11 +2,16 @@ package com.example.ToDoListApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.example.ToDoListApplication")
+@EnableJpaRepositories("com.example.ToDoListApplication")
+@EntityScan("com.example.ToDoListApplication")
 public class ToDoListApplicationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoListApplicationApplication.class, args);
 	}
-
 }
